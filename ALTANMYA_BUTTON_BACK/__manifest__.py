75cb648eb6ce2aaaeed1e9f092137d70e770accf
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-05-01 13:37:20 +0300

    Add files via upload

---
+++ ALTANMYA_BUTTON_BACK/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'ALTANMYA Button Back',
+    'version': '1.0',
+    'summary': 'ALTANMYA Button Back',
+    'sequence': 11,
+    'author': 'ALTANMYA - TECHNOLOGY SOLUTIONS',
+    'company': 'ALTANMYA - TECHNOLOGY SOLUTIONS Part of ALTANMYA GROUP',
+    'website': "http://tech.altanmya.net",
+    'description': """ALTANMYA Button Back """,
+    'category': 'Productivity',
+    'depends': ['website','survey'],
+    'data': [
+        'views/report_customer_collection_rate.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ ALTANMYA_BUTTON_BACK/models/survey_button.py
@@ -0,0 +1,7 @@
+from odoo import api, fields, models, tools, _
+import json
+
+
+class ResPartnerIng(models.Model):
+    _inherit = 'survey.survey'
+
